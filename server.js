97179c961a8234f4b763bed96dc5f3023cd9839a
Shashank-MM <81343256+Shashank-MM@users.noreply.github.com>
2023-12-13 18:44:06 +0530

    updated to HTTP server code

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 // server.js
-const https = require("https");
+const http = require("http");
 const fs = require("fs");
 
-const options = {
-  key: fs.readFileSync("./private_file/private-key.pem"),
-  cert: fs.readFileSync("./private_file/certificate.pem"),
-};
+// const options = {
+//   key: fs.readFileSync("./private_file/private-key.pem"),
+//   cert: fs.readFileSync("./private_file/certificate.pem"),
+// };
 
-const server = https.createServer(options, (req, res) => {
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello, this is your secure server!\n");
+// });
+
+// const PORT = process.env.PORT || 443;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
+const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, this is your secure server!\n");
+  res.end("Hello, this is your server!\n");
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = 3000; // Choose a port number
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
