d4345ade859e77e8b5760b1661c4ad471333452a
Shashank-MM <81343256+Shashank-MM@users.noreply.github.com>
2023-12-13 16:55:32 +0530

    Add initial HTTPS server code

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("./private_file/private-key.pem"),
+  cert: fs.readFileSync("./private_file/certificate.pem"),
+};
+
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, this is your secure server!\n");
+});
+
+const PORT = process.env.PORT || 443;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
